dc821204cad2cf3e6d827d85fbdc02d5facea95a
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-07-09 23:07:53 +0600

    feat: update next.config.ts to include basePath and production environment handling

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "/swiftpay";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  basePath: isProd ? repoName : "", // required for images to work properly in production (github pages)
   allowedDevOrigins: [
     process.env.ALLOWED_DEV_ORIGIN || "http://localhost:3000",
   ],
